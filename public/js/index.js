ed35f43a9e74101fcbe1a044c8e30f3fb1ed97b1
Jim <jimkernicky@gmail.com>
2025-10-29 08:39:31 -0400

    Create issues for listeners and add notes in index.js

--- public/js/index.js
+++ public/js/index.js
@@ -76,20 +76,25 @@ form.addEventListener('submit', e => {
 });
 
 // 3. Clear save searches and related buttons from the DOM
+// Issue #7: Create function for clear-searches button
 clearSearches.addEventListener('click', (e) => {
   console.log('clear-searches button clicked');
 });
 
 // 4. Load More button fetch (uses fetchFromButtons)
+// Issue #8: Create function to fetch more results for current search
 loadMore.addEventListener('click', () => {
   console.log('load-more button clicked');
 })
 
 // 5. Search terms fetch
+// Issue #9: Create function to fetch more results for search phrases buttons
 
 // 6. Search images grid: Save and Remove buttons
+// Issue #6: Create function to remove a result card from the DOM
 
 // 7. Search images grid: open image in modal on image click
+// Issue #5: Create functionality for modal
 
 // 8. Modal listeners: Close modal on click of: 1. close button, 2. window
 
