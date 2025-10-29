d36fa8c0f39503d1bdf7ef03f425e99f7f41c65b
Jim <jimkernicky@gmail.com>
2025-10-29 11:48:32 -0400

    refactor onPageVisits to be more readable

--- public/js/ui/initPage.js
+++ public/js/ui/initPage.js
@@ -11,13 +11,16 @@ const loadMore = document.getElementById('load-more');
 // 1. For Home page DOMContentLoaded listener
 export function onPageVisits() {
   if (!getLocalStorage('search-phrases') || !getLocalStorage('fetched-search-results') ) {
+
     setLocalStorage('search-phrases', []);
+
     clearSearches.classList.add('none');
     loadMore.classList.add('none');
     resultsTitle.classList.add('none');
   } else {
     const savedSearches = getLocalStorage('search-phrases');
     const images = getLocalStorage('fetched-search-results');
+    
     searchTerms.textContent = '';
 
     addSearchTerm(searchTerms, savedSearches);
