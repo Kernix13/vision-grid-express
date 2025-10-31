5c5243815e0b7cca39d768e8b2dd95ced44a8121
Jim <jimkernicky@gmail.com>
2025-10-31 07:13:53 -0400

    bring in elements to board.js, setup for event listeners

--- public/js/board.js
+++ public/js/board.js
@@ -1,3 +1,30 @@
 "use strict";
 
-console.log('this is board.js')
+import { getLocalStorage, setLocalStorage } from "./utils/localStorage.js";
+import { menuButton } from "./ui/menu.js";
+
+const settingsForm = document.getElementById('settings-form');
+const settingsBtn = document.getElementById('settings-btn');
+const imgTextContainer = document.getElementById('img-text-container');
+const thumbnails = document.querySelector('.thumbnails');
+const thumbnailsBtn  = document.getElementById('thumbnails-btn');
+const thumbnailImages = thumbnails.querySelectorAll('.thumb-image');
+
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+/**
+ * * EVENT LISTENERS
+*/
+
+// 1. Load saved images on DOMContentLoaded
+
+// 2. Show/Hide settings form
+
+// 3. Show/hide thumbnails
+
+// 4. Save editable text to local storage
+
+// 5. Open/close hamburger menu
+
+// 6. ???
