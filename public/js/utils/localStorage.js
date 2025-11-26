6715ed91aa8d216b2c5361af27536c12e92fff2b
Jim <jimkernicky@gmail.com>
2025-11-25 21:53:14 -0500

    add return keyword to localStorage functions

--- public/js/utils/localStorage.js
+++ public/js/utils/localStorage.js
@@ -1,21 +1,20 @@
 export function setLocalStorage(str, val) {
-  localStorage.setItem(str, JSON.stringify(val));
+  return localStorage.setItem(str, JSON.stringify(val));
 }
 
 export function getLocalStorage(str) {
-  JSON.parse(localStorage.getItem(str));
+  return JSON.parse(localStorage.getItem(str));
 } 
 
 export function removeLocalStorage(str) {
-  localStorage.removeItem(str);
+  return localStorage.removeItem(str);
 } 
 
 export function incrementSearchPage(searchTerm) {
   const searchPhrasesPage = getLocalStorage('search-phrases-page');
   const searchPhrase = searchPhrasesPage.find(obj => obj.search === searchTerm);
-  
   searchPhrase.page += 1;
-  setLocalStorage('search-phrases-page', searchPhrasesPage);
 
+  setLocalStorage('search-phrases-page', searchPhrasesPage);
   return searchPhrase.page;
 }
